b79d422b68ab4312011a0fe573ddd804ac840a05
Jesse Anacleto <janacleto@csumb.edu>
2018-09-29 08:04:12 +0000

    Re-commiting the index.php file for lab_2

--- Labs/Lab_2/777/index.php
+++ Labs/Lab_2/777/index.php
@@ -16,16 +16,8 @@
                 play();
             ?>
             <form>
-                <button type="button" onclick="">Recombine</button><!--The Ajax Documentation looked like dakr magic to me-->
+                <input type="submit" value="Spin"/>
             </form>
         </div>
-        <footer>
-            <hr>
-            CST336. 2017&copy; Anacleto <br />
-            <strong>Disclaimer:</strong> All material on this website was taken from online resources. <br />
-            It is used for academic purposes only. <br />
-            Other Disclaimer:  The flowers and the prices are real though!  <br />
-            <img src="img/csumblogo.png" alt="CSUMB logo" title="This is the csumb logo"/>
-        </footer>
     </body>
-</html>
+</html> 
