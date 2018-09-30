c6729cefcc0f775cddd3afc9fc3daf028796d3c5
Jesse Anacleto <janacleto@csumb.edu>
2018-09-30 09:53:51 +0000

    completed the winnerCalc Function

--- Labs/Lab_3/priv_silv_jack/index.php
+++ Labs/Lab_3/priv_silv_jack/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <?php
     include "inc/functions.php"; 
-
 ?>
 
 <html>
@@ -16,6 +15,7 @@
     </header>
     
     <body>
+        <h1>Super Silver Jack!! Kingdom Hearts Style!! <hr></h1>
         
         <div class="hand">
         <?php
@@ -25,18 +25,22 @@
             for ($i = 0; $i < 4; $i++) {
                 dealCards($i);
             }
-            print_r($totals);
+            winnerCalc();
+            //echo"totals:";
+            //print_r($totals);
+            //echo "<br/>";
+            //print_r($diff);
         ?>
         </div>
         
         <footer>
             <div>
                 CST 336 2018 &copy; Baird, Chi, Hernandez and Anacleto<br/>
-                <strong>Disclaimer:</strong> I did not create any of the characters 
-                on this site. All images belong to the Kingdom Hearts franchise and 
-                all rights belong to Disney and Square Enix.
+                <strong>Disclaimer:</strong> We did not create any of the characters 
+                on this site. 
+                <br/>All images belong to the Kingdom Hearts franchise and 
+                all rights belong to <br/>Disney and Square Enix.
             </div>
-                
                 <img src="img/csumblogo.png" alt="CSUMB logo" title="This is the CSUMB logo"/>
         </footer>
     </body>
